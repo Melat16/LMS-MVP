fce94fd38a6c665fa5affdcc5da89b9334eb1222
melat <melatkassaye13@gmail.com>
2023-01-13 14:44:45 +0300

    footer and who we are added

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,15 @@
 import Navbar from "../components/Navbar";
 import Home2 from "./home";
-
+import Footer from "../components/Footer";
+import Who from "./who";
 
 export default function Home() {
   return (
     <>
      <Navbar />
      <Home2/>
-     
-    
+     <Who/>
+     <Footer/>
    </>
 
   )
