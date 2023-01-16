4de3a13303d08b3076d80000d3880acda2d5343d
melat <melatkassaye13@gmail.com>
2023-01-16 14:05:50 +0300

    courses added

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,13 @@ import Navbar from "../components/Navbar";
 import Home2 from "./home";
 import Footer from "../components/Footer";
 import Who from "./who";
-
+import Courses from "./courses";
 export default function Home() {
   return (
     <>
      <Navbar />
      <Home2/>
+    <Courses/>
      <Who/>
      <Footer/>
    </>
